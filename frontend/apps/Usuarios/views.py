3b8a9e50658dd5393f20300a21808588f3f61c9d
Daniel Caicedo <santiagocaicedo140@gmail.com>
2025-08-11 10:11:42 -0500

    Primer html
    configuracion y creacion del primer archivo html

--- frontend/apps/Usuarios/views.py
+++ frontend/apps/Usuarios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'layout/index.html')
+def login_view(request):
+    return render(request, 'pages/login.html')
