414531f3984b9e30d8532a171c4d91d9dcc2495f
Daniel Caicedo <santiagocaicedo140@gmail.com>
2025-08-13 11:46:01 -0500

    BD-datos

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SGC',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
