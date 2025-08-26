40131c47f011a19af065726617f1222f799e614c
Sluss <jdmapple322@gmail.com>
2025-08-26 12:28:05 -0500

    Next: Viewsets

--- backend/core_apps/usuarios_api/admin.py
+++ backend/core_apps/usuarios_api/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import (Usuario,TipoDocumento,Genero,EstudioFormal,Rol,Estado,
+                    UsuarioManager,TipoCertificado,
+                    FormacionComplementaria,Bitacora,EstadoSolicitud,TipoSolicitud)
 # Register your models here.
 admin.site.register(Usuario)
+admin.site.register(TipoDocumento)
+admin.site.register(Genero)
+admin.site.register(EstudioFormal)
+admin.site.register(Rol)
+admin.site.register(Estado)
+admin.site.register(UsuarioManager)
+admin.site.register(TipoCertificado)
+admin.site.register(FormacionComplementaria)
+admin.site.register(Bitacora)
+admin.site.register(EstadoSolicitud)
+admin.site.register(TipoSolicitud)
