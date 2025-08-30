bd7b9b4258e928ffdcea90d274c5678fc3c460c0
paguatiqn <melissaestefanymerapaguatian@gmail.com>
2025-08-30 17:04:38 -0500

    modulo grupo sena
     grupo sena

--- frontend/apps/GruposSENA/urls.py
+++ frontend/apps/GruposSENA/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('grupo_sena/', views.grupo_sena, name='grupo_sena'),
+    path('new_grupo/', views.new_grupo, name='new_grupo'),
 ]
--- frontend/apps/GruposSENA/views.py
+++ frontend/apps/GruposSENA/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def grupo_sena(request):
     return render(request, 'layout/grupo_sena.html')
+
+def new_grupo(request):   
+    return render(request, 'pages/new_grupo.html')
