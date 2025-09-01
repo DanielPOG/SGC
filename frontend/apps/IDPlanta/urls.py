cce3a70179f63b260aac47493d7e3f18e17e07c8
paguatiqn <melissaestefanymerapaguatian@gmail.com>
2025-09-01 11:25:40 -0500

    idp
    idp avances

--- frontend/apps/IDPlanta/urls.py
+++ frontend/apps/IDPlanta/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('id_planta/', views.id_planta, name='id_planta'),
+
+    path('newid_planta', views.newid_palnta, name='newid_planta'),
 ]
--- frontend/apps/IDPlanta/views.py
+++ frontend/apps/IDPlanta/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def id_planta(request):
     return render(request, 'layout/id_planta.html')
+def newid_palnta(request):
+    return render(request, 'pages/newid_planta.html')
