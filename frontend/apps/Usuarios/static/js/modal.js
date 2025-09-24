d79d2ee829f93ed280bbcae6a9b68067d582279b
paguatiqn <melissaestefanymerapaguatian@gmail.com>
2025-09-24 00:40:23 -0500

    ,
    ,

--- frontend/apps/Usuarios/static/js/modal.js
+++ frontend/apps/Usuarios/static/js/modal.js
@@ -1,51 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("JS cargado ✅");
-
-  // --- LOGIN ---
-  const form = document.querySelector(".login-form");
-
-  if (!form) {
-    console.error("⚠️ No se encontró el formulario .login-form");
-    return;
-  }
-
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    console.log("Evento submit detectado ✅");
-
-    const usuario = document.getElementById("usuario").value.trim();
-    const password = document.getElementById("password").value.trim();
-
-    if (!usuario || !password) {
-      alert("⚠️ Debes ingresar usuario y contraseña");
-      return;
-    }
-
-   try {
-        const response = await fetch("http://127.0.0.1:8001/api/usuarios/login/", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-           body: JSON.stringify({ correo: usuario, password }),
-        });
-        
-        if (response.ok) {
-            const data = await response.json();
-            alert("✅ Login exitoso");
-            console.log("Tokens:", data);
-            localStorage.setItem("access", data.access);
-            localStorage.setItem("refresh", data.refresh);
-            // Esta es la línea que realiza la redirección
-            window.location.href = "/principal/";
-        } else {
-            const error = await response.json();
-            alert("❌ Error: " + (error.detail || "Credenciales inválidas"));
-        }
-    } catch (err) {
-        alert("⚠️ Error de conexión con el servidor");
-        console.error(err);
-    }
-  });
-    // Abrir modal
+   // Abrir modal
     document.getElementById('openModal').onclick = function(e) {
       e.preventDefault();
       document.getElementById('modalRecuperar').style.display = 'flex';
@@ -63,6 +16,3 @@ document.addEventListener("DOMContentLoaded", () => {
         modal.style.display = "none";
       }
     }
-
-});
-
